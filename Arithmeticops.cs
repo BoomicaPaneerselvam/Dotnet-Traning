3b7b6d50496db83377c07ebd58a2bec7afd33447
BoomicaPaneerselvam <124019606+BoomicaPaneerselvam@users.noreply.github.com>
2023-01-31 11:00:10 +0530

    Add files via upload

---
+++ Arithmeticops.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    abstract class Arithmeticops
+    {
+        public abstract void arithmeticoperation();
+
+    }
+    class  Calculator:Arithmeticops
+    {
+        public override void arithmeticoperation()
+        {
+            Console.WriteLine("Enter two number for calculation ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Addition of two number is:" + (a + b));
+            Console.WriteLine("Subtraction of two Number is:" + (a - b));
+            Console.WriteLine("Multiplication of two number is:" + (a * b));
+            Console.WriteLine("Division of two Number is:" + (a / b));
+        }
+        public static void Main(string[] args)
+        {
+            Calculator cal= new Calculator();
+           cal.arithmeticoperation();
+        }
+    }
+}
---
+++ Membership.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    interface Award_winners
+    {
+        void silver_winners();
+        void Gold_winners();
+        void Platinum_winners();
+    }
+    class Membership_package : Award_winners
+    {
+        public void silver_winners()
+        {
+            Console.WriteLine("Silver \n 3 Days accomadation at resort");
+        }
+
+        public void Gold_winners()
+        {
+            Console.WriteLine("Gold \n 5 days accomadation at resort \n 2 Dinners on the house ");
+        }
+        public void Platinum_winners()
+        {
+            Console.WriteLine("Platinum \n 7 days accomadation at resort \n 5 Dinners on the house");
+        }
+
+
+
+        public static void Main(string[] args)
+        {
+            Membership_package mp = new Membership_package();
+            mp.silver_winners();
+            mp.Gold_winners();
+            mp.Platinum_winners();
+
+        }
+    }
+}
+    
+
+
