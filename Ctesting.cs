d3a1c0ee51e6ad35e8af08dc3ef83a00d2d79eca
BoomicaPaneerselvam <124019606+BoomicaPaneerselvam@users.noreply.github.com>
2023-01-31 10:52:13 +0530

    Add files via upload

---
+++ Ctesting.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CORONA_TESTING
+{
+    internal class Ctesting
+    {
+        public void testing()
+        {
+            Console.WriteLine("Do you have travel history?:");
+            var answer = Console.ReadLine();
+
+            if (answer!="yes")            
+                Console.WriteLine("Safe ! Not COVID-19 ");            
+             else
+            {
+                Console.WriteLine("DO you have temperature?");
+                var ans2 = Console.ReadLine();
+                if(ans2!="yes")
+                {
+                    Console.WriteLine("Home Quarantine for 28 days");
+                }
+                else
+                {
+                    Console.WriteLine("Do you have cough/sneeze?");
+                    var ans3 = Console.ReadLine();
+                    if (ans3 != "yes")
+                        Console.WriteLine("Quarentine fever medicine");
+                    else
+                        Console.WriteLine("Swab test");
+
+                }
+            }
+           
+        }
+    }
+    class Implementation
+    {
+        public static void Main()
+        {
+            Ctesting c1 = new Ctesting();
+            c1.testing();
+            
+
+        }
+    }
+}
---
+++ EB_bill.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ebill
+{
+    internal class EBbill
+    {
+        public void Unitcaculation(int units)
+        {
+            int payment;
+            if (units <= 200)
+            {
+                payment = 2 * units;
+                Console.WriteLine("Your EB_bill is {0}", payment);
+            }
+            if (units > 200 && units < 350)
+            {
+                payment = 200 * 2 + (units - 200) * 3;
+                Console.WriteLine("Your EB_bill is {0}", payment);
+            }
+            if (units > 350 && units < 500)
+            {
+                payment = 200 * 2 + 150 * 3 + (units - 350) * 5;
+                Console.WriteLine("Your EB_bill is {0}", payment);
+            }
+
+        }
+        public static void Main(string[] args)
+        {
+            EBbill bill = new EBbill();
+            Console.WriteLine("Enter no of units");
+            int units = Convert.ToInt32(Console.ReadLine());
+            bill.Unitcaculation(units);
+
+        }
+
+    }
+}
