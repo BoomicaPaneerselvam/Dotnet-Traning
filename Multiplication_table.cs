e1a70483979a9369afe933654f59a4029e5c361b
BoomicaPaneerselvam <124019606+BoomicaPaneerselvam@users.noreply.github.com>
2023-01-31 11:05:47 +0530

    Add files via upload

---
+++ Multiplication_table.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplicationtable
+{
+    internal class Multiplication_table
+    {
+         
+            public Multiplication_table(int num)
+            {
+                for (int i = 1; i <= 20; i++)
+                {
+                    int ans = num * i;
+                    Console.WriteLine( num+"*"+i + "=" + ans);
+                }
+            }
+            public static void Main()
+            {
+            Console.WriteLine("Enter number");
+           int n = Convert.ToInt32(Console.ReadLine());
+            Multiplication_table mt = new Multiplication_table(n);
+            }
+        }
+
+    }
